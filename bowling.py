47f25f71a86e97b61cd6c506ff5187429ddc9820
yadlra <yadi.sb1@gmail.com>
2019-07-12 13:21:58 +0100

    Set up files and unittest

---
+++ bowling.py
@@ -0,0 +1,2 @@
+class BowlingGame:
+    pass 
---
+++ test_bowling.py
@@ -0,0 +1,10 @@
+import unittest
+import bowling
+
+class TestBowling(unittest.TestCase):
+
+    def setUp(self):
+        self.game = bowl.BowlingGame()
+
+if __name__ == '__main__':
+    unittest.main()
