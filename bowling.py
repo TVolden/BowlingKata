df9cc0babde1477d1555f7e54fcd228782566637
yadlra <yadi.sb1@gmail.com>
2019-07-12 13:32:01 +0100

    Pass first test for roll and score

--- bowling.py
+++ bowling.py
@@ -1,2 +1,10 @@
 class BowlingGame:
-    pass 
+
+    def __init__(self):
+        self._score = 0
+
+    def roll(self, pins):
+        self._score += pins
+
+    def score(self):
+        return self._score
--- test_bowling.py
+++ test_bowling.py
@@ -1,10 +1,18 @@
 import unittest
-import bowling
+import bowling as bowl
 
 class TestBowling(unittest.TestCase):
 
     def setUp(self):
         self.game = bowl.BowlingGame()
+
+    def test_roll_all_gutter(self):
+        self.roll_many(20, 0)
+        self.assertEqual(self.game.score(), 0)
+
+    def roll_many(self, rolls, pins):
+        for roll in range(rolls):
+            self.game.roll(pins)
 
 if __name__ == '__main__':
     unittest.main()
